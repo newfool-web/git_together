cd3f0270392be35e5092e8180cd56c78753f9899
Achyut Gupta <achyut10gupta@gmail.com>
2025-12-16 20:32:02 +0530

    Schemas, Basic Route and app setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectDB = require("../src/config/database.js");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+const authRouter = require("./routes/auth.js")
+const profileRouter = require("./routes/profile.js")
+app.use("/", authRouter);
+app.use("/", profileRouter);
+
+connectDB()
+.then(()=>{
+    console.log("Connected to Database");
+    app.listen(3000,()=>{
+        console.log("Server is Listening on port 3000");
+    });
+})
+.catch((err)=>{
+    console.log("Failed to Connect to Database! "+ err.message);
+});
+
