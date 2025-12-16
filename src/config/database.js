8259035065b77ef2aa30aab8aa122111a74c65d8
Achyut Gupta <achyut10gupta@gmail.com>
2025-12-16 20:30:23 +0530

    Schemas, Basic Routes, and app setup

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () =>{    
-    await mongoose.connect("mongodb+srv://achyut10gupta_db_user:12345678-a@cluster0.0itegxr.mongodb.net/dev-Tinder?appName=Cluster0")
-}
-
-module.exports = connectDB;
-
