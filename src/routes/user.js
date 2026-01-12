b5d601570fe1c9ddb90baa138b8f8a1a99547145
Achyut Gupta <achyut10gupta@gmail.com>
2026-01-13 02:52:51 +0530

    feed route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { userAuth } = require('../middleware/auth');
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest.js")
+const User = require("../models/user.js")
 
 //Get all the pending connection request for the LoggedIn User
 userRouter.get("/user/requests/received", userAuth, async (req, res)=>{
@@ -50,6 +51,55 @@ userRouter.get("/user/connections", userAuth, async (req, res)=>{
     }
     catch(err){
         res.status(400).send("Error: "+err.message)
+    }
+})
+
+userRouter.get("/user/feed", userAuth, async (req, res)=>{
+
+    //User should see all the cards except:
+    //1. His own card
+    //2. His connections (Status: Accepted)
+    //3. Already send connection req(status: interested)
+    //4. Ignored peoples (Status: Ignored)
+    //5. Rejected People(Status: Rejected)
+    //Means ek tarah se aisa ho gya ki agar connectionRequest document(db) mein entry 
+    // ho gyi toh wo undono ko feed mein nhi dikhna chahiye. Mtlb fromUserId 
+    // ya toUserId mein loggedinUser nhi hona chahiye.
+    try{
+        const loggedInUser = req.user;
+        let limit = parseInt(req.query.limit)||10;
+        limit = limit>50?50:limit
+        const page = parseInt(req.query.page) ||1;
+        const skip = (page-1)*limit
+
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        console.log(connectionRequest)
+
+        const hideUsersFromFeed = new Set();
+        connectionRequest.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId.toString())
+            hideUsersFromFeed.add(req.toUserId.toString())
+        })
+
+        const users = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select("firstName lastName photoUrl age gender about skills").skip(skip).limit(limit)
+        res.json({
+            data: users
+        })
+    }
+    catch(err){
+        res.status(400).json({message: "Error: "+err.message})
     }
 })
 
