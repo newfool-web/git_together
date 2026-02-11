7c5f7641d9c5edc2e9693e63146b05d93df13c9f
Achyut Gupta <achyut10gupta@gmail.com>
2026-02-12 04:56:33 +0530

    dep error fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () =>{    
-    await mongoose.connect(process.env.DB_CONNECTION_SECRET)
-}
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.DB_CONNECTION_SECRET, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
 
 module.exports = connectDB;
-
